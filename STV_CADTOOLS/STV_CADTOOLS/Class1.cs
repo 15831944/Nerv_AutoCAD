d59ed51bad02d7d860acb28e9362376979c4706d
lmengfan <41481152+lmengfan@users.noreply.github.com>
2020-05-11 21:27:44 -0400

    initial commit

---
+++ STV_CADTOOLS/STV_CADTOOLS/Class1.cs
@@ -0,0 +1,50 @@
+﻿//Read a Text File
+using System;
+using System.IO;
+
+namespace readwriteapp
+{
+    using System.Windows;
+
+    using System.Collections.Generic;
+    class ReadCSV
+    {
+        [STAThread]
+        public static IDictionary<string, string> Read(string file)
+        {
+            IDictionary<string, string> dict = new Dictionary<string, string>();
+            String line;
+
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(file);
+
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    string[] ss = line.Split(',');
+                    //append line to list
+                    dict.Add(ss[0], ss[1]);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+                return dict;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+        }
+    }
+
+
+
+}
---
+++ STV_CADTOOLS/STV_CADTOOLS/Commands.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using AcAp = Autodesk.AutoCAD.ApplicationServices.Application;
+using System.Windows.Forms;
+
+namespace STV_CADTOOLS
+{
+    public class Commands
+    {
+        [CommandMethod("TEST")]
+        public void Test()
+        {
+            var doc = AcAp.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
+            var ed = doc.Editor;
+            using (var tr = db.TransactionManager.StartTransaction())
+            {
+                MessageBox.Show("Debug 3");
+                tr.Commit();
+            }
+        }
+        [CommandMethod("RenameLayers")]
+        public static void RenameLayer()
+        {
+            // Get the current document and database
+            Document acDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Returns the layer table for the current database
+                LayerTable acLyrTbl;
+                acLyrTbl = acTrans.GetObject(acCurDb.LayerTableId,
+               OpenMode.ForWrite) as LayerTable;
+                // Clone layer 0 (copy it and its properties) as a new layer
+                LayerTableRecord acLyrTblRec;
+
+                IDictionary<string, string> dict = null;
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.InitialDirectory = "c:\\";
+                    openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    openFileDialog.FilterIndex = 2;
+                    openFileDialog.RestoreDirectory = true;
+                    List<string> fileContent = new List<string>();
+                    if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        //read file
+                        dict = readwriteapp.ReadCSV.Read(openFileDialog.FileName);
+                    }
+                }
+                foreach(string old in dict.Keys)
+                {
+                    try 
+                    { 
+                        acLyrTblRec = acTrans.GetObject(acLyrTbl[old], OpenMode.ForWrite) as LayerTableRecord;
+                        // Change the name of the cloned layer
+                        acLyrTblRec.UpgradeOpen();
+                        acLyrTblRec.Name = dict[old];
+                    }
+                    catch
+                    {
+                        
+                    }
+                }
+
+
+                acTrans.Commit();
+            }
+        }
+        [CommandMethod("RenameLayer")]
+        public static void RenameLayers()
+        {
+            // Get the current document and database
+            Document acDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Returns the layer table for the current database
+                LayerTable acLyrTbl;
+                acLyrTbl = acTrans.GetObject(acCurDb.LayerTableId,
+               OpenMode.ForWrite) as LayerTable;
+                // Clone layer 0 (copy it and its properties) as a new layer
+                LayerTableRecord acLyrTblRec;
+                acLyrTblRec = acTrans.GetObject(acLyrTbl["0"],
+               OpenMode.ForRead).Clone() as
+               LayerTableRecord;
+                // Change the name of the cloned layer
+                acLyrTblRec.Name = "MyLayer";
+                // Add the cloned layer to the Layer table and transaction
+                acLyrTbl.Add(acLyrTblRec);
+                acTrans.AddNewlyCreatedDBObject(acLyrTblRec, true);
+                // Save changes and dispose of the transaction
+                acTrans.Commit();
+            }
+        }
+    }
+
+
+
+
+}
